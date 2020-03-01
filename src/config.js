456c194850f885bd07b9b39f0946b32094e78874
Sam Cross <crossmansam@gmail.com>
2020-03-01 01:44:10 +0000

    chore: add additional info in config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
+// for local device ONLY
 export const API_URL = 'http://localhost:3001';
-// for other devices on local network: 192.168.1.241:3001
+// for other devices on local network (replace with your local or public IP)
+// export const API_URL = 'http://192.168.1.241:3001';
